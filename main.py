2161cfd50c2e7e1893273f711fda5f910c7615b2
155792 <pgw1216@naver.com>
2022-12-08 14:37:32 +0900

    221208 14:37 Macro 시작버튼 추가시 창 추가

--- main.py
+++ main.py
@@ -3,9 +3,103 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVB
     QLineEdit, QLabel, QCheckBox, QListWidget
 from PyQt5.QtCore import Qt
 
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+
+    def initUI(self):
+        self.setWindowTitle("Armko Macro")
+        self.resize(450, 350)
+
+        # geo = self.geometry()
+
+        add_button = QPushButton('Macro 추가')
+        edit_button = QPushButton('Macro 편집')
+        copy_button = QPushButton('Macro 복제')
+        delect_button = QPushButton('Macro 삭제')
+
+        add_button.clicked.connect(self.add_macro)
+
+        edit_button.clicked.connect(self.edit_macro)
+
+        self.macro_start_radio = QRadioButton("매크로 시작")
+        self.macro_start_radio.setObjectName("macro_start_radio")
+
+        self.macro_stop_radio = QRadioButton("매크로 중지")
+        self.macro_stop_radio.setObjectName("macro_stop_radio")
+
+        #layout_1 = start & Stop layout
+        self.h_box_layout_1 = QHBoxLayout()
+        self.h_box_layout_1.addWidget(self.macro_start_radio)
+        self.h_box_layout_1.addWidget(self.macro_stop_radio)
+        self.h_box_layout_1.addStretch(2)
+
+        #layout2 = label과 push버튼을 포함한 layout
+        self.h_box_layout_2 = QHBoxLayout()
+        #layout2에 vbox1을 생성하여 label 생성
+        self.v_box_1_inside_layout_2 = QVBoxLayout()
+        self.macro_list = QListWidget()
+        #self.macro_label.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        self.macro_list.setWordWrap(True)
+        self.macro_list.setStyleSheet("QListWidget"
+                                      "{"
+                                      "border : 4px solid black;"
+                                      "background : white;"
+                                      "}")
+        self.macro_list.setGeometry(0, 0, 1000, 1000)
+        self.macro_list.insertItem(0, "매크로 1")
+        self.v_box_1_inside_layout_2.addWidget(self.macro_list)
+
+        #layout2에 vbox2를 생성하여 push버튼 삽입
+        self.v_box_2_inside_layout_2 = QVBoxLayout()
+        self.v_box_2_inside_layout_2.setSpacing(0)
+        self.v_box_2_inside_layout_2.addWidget(add_button)
+        self.v_box_2_inside_layout_2.addWidget(edit_button)
+        self.v_box_2_inside_layout_2.addWidget(copy_button)
+        self.v_box_2_inside_layout_2.addWidget(delect_button)
+
+        self.v_box_2_inside_layout_2.addStretch(0)
+        self.v_box_2_inside_layout_2.setSpacing(10)
+        self.v_box_2_inside_layout_2.setContentsMargins(0, 0, 0, 0)
+
+        self.h_box_layout_2.addLayout(self.v_box_1_inside_layout_2, 3)
+        self.h_box_layout_2.addLayout(self.v_box_2_inside_layout_2, 1)
+
+        # layout3 = 체크박스를 포함한 layout
+        self.h_box_layout_3 = QHBoxLayout()
+        self.float_top = QCheckBox("최상단에 띄우기")
+        self.float_top.setObjectName("float_top")
+        self.h_box_layout_3.addWidget(self.float_top)
+
+        #wrapper를 통해 모든 layout을 넣어 실행
+        self.wrapper = QVBoxLayout()
+        self.wrapper.setAlignment(Qt.AlignTop)
+        self.wrapper.addLayout(self.h_box_layout_1)
+        self.temp_widget = QWidget()
+        self.temp_widget.setLayout(self.h_box_layout_2)
+        self.wrapper.addWidget(self.temp_widget, 10)
+        self.wrapper.addLayout(self.h_box_layout_3)
+        self.macro_stop_radio.toggle()
+        self.setLayout(self.wrapper)
+
+        self.show()
+
+    def add_macro(self):
+        self.add_macro_window = AddAndEditMacroWindow("Add Macro")
+
+        self.add_macro_window.show()
+
+    def edit_macro(self):
+        self.edit_macro_window = AddAndEditMacroWindow("Edit Macro")
+
+        self.edit_macro_window.show()
+
 
 class AddAndEditMacroWindow(QWidget):
-
+#Myapp(Qwidget)의 상속 class 시작
     def __init__(self, title_str):
         super().__init__()
         self.initWindow(title_str)
@@ -18,9 +112,7 @@ class AddAndEditMacroWindow(QWidget):
         self.h_box_layout_1 = QHBoxLayout()
 
         label1 = QLabel('F12키를 통해 현재 마우스 위치를 Macro 항목에 추가합니다. \n저장된 순서에 따라 Macro가 순차적으로 실행됩니다.', self)
-
         label1.setAlignment(Qt.AlignLeft)
-
         self.h_box_layout_1.addWidget(label1)
 
         # font1 = label1.font()
@@ -31,6 +123,7 @@ class AddAndEditMacroWindow(QWidget):
         mouse_button = QPushButton('마우스 추가')
         keyboard_button = QPushButton('키보드 추가')
         delay_button = QPushButton('지연 추가')
+
 
         self.h_box_layout_2 = QHBoxLayout()
         self.v_box_1_inside_layout_2 = QVBoxLayout()
@@ -71,114 +164,39 @@ class AddAndEditMacroWindow(QWidget):
 
         self.setLayout(self.wrapper)
 
-        #asdf
+        start_button.clicked.connect(self.start_macro)
+
+    def start_macro(self):
+        self.start_macro_window = Startsetting("Start setting")
+
+        self.start_macro_window.show()
 
 
-class MyApp(QWidget):
+class Startsetting(QWidget):
 
-    def __init__(self):
+    def __init__(self, title_str1):
         super().__init__()
-        self.initUI()
+        self.initWindow(title_str1)
 
 
-    def initUI(self):
-        self.setWindowTitle("Armko Macro")
-        self.resize(450, 350)
-
-        # geo = self.geometry()
-
-        add_button = QPushButton('Macro 추가')
-        edit_button = QPushButton('Macro 편집')
-        copy_button = QPushButton('Macro 복제')
-        delect_button = QPushButton('Macro 삭제')
-
-        add_button.clicked.connect(self.add_macro)
-
-        edit_button.clicked.connect(self.edit_macro)
-
-        self.macro_start_radio = QRadioButton("매크로 시작")
-        self.macro_start_radio.setObjectName("macro_start_radio")
-
-        self.macro_stop_radio = QRadioButton("매크로 중지")
-        self.macro_stop_radio.setObjectName("macro_stop_radio")
-
+    def initWindow(self, title_str1):
+        self.setWindowTitle(title_str1)
+        self.resize(450, 400)
 
         self.h_box_layout_1 = QHBoxLayout()
-        self.h_box_layout_1.addWidget(self.macro_start_radio)
-        self.h_box_layout_1.addWidget(self.macro_stop_radio)
-        self.h_box_layout_1.addStretch(2)
 
-        #박건우 멍청이
+        label1 = QLabel('Macro 시작 버튼을 설정하세요. \n반복 횟수가 0일 경우 중지버튼을 누르기 전까지 실행됩니다..', self)
+        label1.setAlignment(Qt.AlignLeft)
+        self.h_box_layout_1.addWidget(label1)
 
-        self.h_box_layout_2 = QHBoxLayout()
 
-        self.v_box_1_inside_layout_2 = QVBoxLayout()
-        self.macro_list = QListWidget()
-        #self.macro_label.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-        self.macro_list.setWordWrap(True)
-
-        self.macro_list.setStyleSheet("QListWidget"
-                                      "{"
-                                      "border : 4px solid black;"
-                                      "background : white;"
-                                      "}")
-
-        self.macro_list.setGeometry(0, 0, 1000, 1000)
-
-        self.macro_list.insertItem(0, "매크로 1")
-
-        self.v_box_1_inside_layout_2.addWidget(self.macro_list)
-        # asdf
-        self.v_box_2_inside_layout_2 = QVBoxLayout()
-        self.v_box_2_inside_layout_2.setSpacing(0)
-        self.v_box_2_inside_layout_2.addWidget(add_button)
-        self.v_box_2_inside_layout_2.addWidget(edit_button)
-        self.v_box_2_inside_layout_2.addWidget(copy_button)
-        self.v_box_2_inside_layout_2.addWidget(delect_button)
-
-        self.v_box_2_inside_layout_2.addStretch(0)
-        self.v_box_2_inside_layout_2.setSpacing(10)
-        self.v_box_2_inside_layout_2.setContentsMargins(0, 0, 0, 0)
-
-        self.h_box_layout_2.addLayout(self.v_box_1_inside_layout_2, 3)
-        self.h_box_layout_2.addLayout(self.v_box_2_inside_layout_2, 1)
-
-        self.h_box_layout_3 = QHBoxLayout()
-
-        self.float_top = QCheckBox("최상단에 띄우기")
-        self.float_top.setObjectName("float_top")
-
-        self.h_box_layout_3.addWidget(self.float_top)
 
         self.wrapper = QVBoxLayout()
-        self.wrapper.setAlignment(Qt.AlignTop)
+
 
         self.wrapper.addLayout(self.h_box_layout_1)
 
-        self.temp_widget = QWidget()
-
-        self.temp_widget.setLayout(self.h_box_layout_2)
-
-        self.wrapper.addWidget(self.temp_widget, 10)
-        #
-        self.wrapper.addLayout(self.h_box_layout_3)
-
-        self.macro_stop_radio.toggle()
-
         self.setLayout(self.wrapper)
-
-        self.show()
-
-    def add_macro(self):
-        self.add_macro_window = AddAndEditMacroWindow("Add Macro")
-
-        self.add_macro_window.show()
-
-    def edit_macro(self):
-        self.edit_macro_window = AddAndEditMacroWindow("Edit Macro")
-
-        self.edit_macro_window.show()
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
